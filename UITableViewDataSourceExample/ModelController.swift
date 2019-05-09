93d1bbbedaf637a8d7d2b66c291f361c5400697c
e-sung <dev.esung@gmail.com>
2019-05-09 20:34:48 +0900

    Bad Version

---
+++ UITableViewDataSourceExample/ModelController.swift
@@ -0,0 +1,14 @@
+//
+//  ModelController.swift
+//  UITableViewDataSourceExample
+//
+//  Created by 류성두 on 09/05/2019.
+//  Copyright © 2019 Sungdoo. All rights reserved.
+//
+
+import Foundation
+
+class ModelController: NSObject {
+    var kittisList = ["🐱", "😹", "😼", "😸", "😽", "😾"]
+    var smileyList = ["😐", "😂", "😏", "😊", "😊", "😠", "😱"]
+}
--- UITableViewDataSourceExample/ViewController.swift
+++ UITableViewDataSourceExample/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // MARK: - View
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var segmentControl: UISegmentedControl!
+    
+    // MARK: - Model
+    let modelController = ModelController()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - Actions
+    @IBAction func segementSelected(_ sender: UISegmentedControl) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if segmentControl.selectedSegmentIndex == 0 {
+            return modelController.kittisList.count
+        }
+        else {
+            return modelController.smileyList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if segmentControl.selectedSegmentIndex == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "catCell", for: indexPath)
+            cell.textLabel?.text = modelController.kittisList[indexPath.row]
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "smileyCell", for: indexPath)
+            cell.textLabel?.text = modelController.smileyList[indexPath.row]
+            return cell
+        }
+    }
 }
 
